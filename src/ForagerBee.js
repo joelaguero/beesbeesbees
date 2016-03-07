d90dbec7ecf0d82430c60b56bd3f2a7d1f84dee0
Joel Aguero <joelmaguero@gmail.com>
2016-03-07 12:05:25 -0800

    add age property and setup subclass/superclass relationship with Bee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,10 @@
 var ForagerBee = function() {
+  HoneyMakerBee.call(this);
+  this.age = 10;
 };
+
+ForagerBee.prototype = Object.create(HoneyMakerBee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+
+
 
