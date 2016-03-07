41c2144c08040bb0f8737222e7bf21bf02eb16d0
Joel Aguero <joelmaguero@gmail.com>
2016-03-07 12:02:35 -0800

    add age, job, and honeypot properties to honeybee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,6 +1,8 @@
 var HoneyMakerBee = function() {
   Bee.call(this);
-  
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
 
 HoneyMakerBee.prototype = Object.create(Bee.prototype);
