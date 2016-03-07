3c8b908d953ddb80f9f5b8d2d7f60a29f934ea94
Joel Aguero <joelmaguero@gmail.com>
2016-03-07 12:12:06 -0800

    add age property and setup sub/super class relationship with foragerBee

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,10 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
 };
+
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+
 
