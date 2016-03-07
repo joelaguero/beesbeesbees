c22ddd04d982ac91da66ff83ad9850116cb8f47b
Joel Aguero <joelmaguero@gmail.com>
2016-03-07 12:06:38 -0800

    add treasureChest, canFly, and job property

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,6 +1,9 @@
 var ForagerBee = function() {
   HoneyMakerBee.call(this);
   this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
 ForagerBee.prototype = Object.create(HoneyMakerBee.prototype);
